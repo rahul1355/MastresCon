8ae437de56e51888e16b1ef5aa2be8c34916f8fc
Rahul <rahul.srikrishana1355@gmail.com>
2020-09-24 16:58:05 +0530

    like commits

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -9,7 +10,22 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? <Spinner /> : <Fragment>test</Fragment>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
@@ -18,7 +34,7 @@ Posts.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  posts: state.post,
+  post: state.post,
 });
 
 export default connect(mapStateToProps, { getPosts })(Posts);
